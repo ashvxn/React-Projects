de366a34412e6a2563308239ca141b20f7dedfea
ashvin <123237841+ashvxn@users.noreply.github.com>
2024-07-13 00:37:27 +0530

    login box 30 percent completed

--- ticket exchanger(ktxp)/src/App.jsx
+++ ticket exchanger(ktxp)/src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import './index.css';
+
+import Login from './login';
+
+function App(){
+ return (
+    <Login/>
+
+
+ )
+}
 
 
 export default App
---
+++ ticket exchanger(ktxp)/src/login.jsx
@@ -0,0 +1,18 @@
+import react from 'react' ;
+
+function Login(){
+    return (
+     <div>
+     <  h1>Login to KTXP Portal</h1>
+        <div className='mainbox'>
+            {/* hheehe */}
+            <input type="text" className='input1'/>
+            <input type="text" className='input1' style={{ top : '48%'}}/>
+            <div className='second_box'></div>
+        </div>
+    </div>
+
+    )
+}
+
+export default Login
