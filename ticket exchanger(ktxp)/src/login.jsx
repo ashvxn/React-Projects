41c1b61b61f3411354824dd84d5f3eb4439ecd8b
ashvin <123237841+ashvxn@users.noreply.github.com>
2024-07-13 12:01:56 +0530

    D

--- ticket exchanger(ktxp)/src/login.jsx
+++ ticket exchanger(ktxp)/src/login.jsx
@@ -1,13 +1,25 @@
 import react from 'react' ;
+import { useState } from 'react'
 
 function Login(){
+    const[user_name, setuser_name] = useState("Username")
+    const[password, set_password] = useState("password")
+    function login_funtion(){
+        
+        document.getElementById('1').value = ""
+        document.getElementById('2').value = ""
+
+
+    }
     return (
      <div>
      <  h1>Login to KTXP Portal</h1>
         <div className='mainbox'>
             {/* hheehe */}
-            <input type="text" className='input1'/>
-            <input type="text" className='input1' style={{ top : '48%'}}/>
+            <span className='welcome'>WELCOME</span>
+            <input type="text" className='input1' id="1" placeholder={user_name}/>
+            <input type="text" className='input2' id ='2' style={{ top : '48%'}} placeholder='password'/>
+            <button className='login' onClick={login_funtion}>Login</button>
             <div className='second_box'></div>
         </div>
     </div>
